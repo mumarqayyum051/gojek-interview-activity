0aae39bc48a5d5eb68c9f5ea87e91fba5d9be921
mumarqayyum051 <mumarqayyum051>
2022-07-30 12:35:17 +0500

    posts designed

--- src/app/modules/admin/feed/feed.component.ts
+++ src/app/modules/admin/feed/feed.component.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/member-ordering */
 import { Component, OnInit } from '@angular/core';
 import { faker } from '@faker-js/faker';
 
@@ -9,6 +10,32 @@ import { faker } from '@faker-js/faker';
 export class FeedComponent implements OnInit {
     today = new Date();
     constructor() {}
+    result: any[] = [
+        {
+            id: 1,
+            name: faker.name.findName(),
+            email: faker.internet.email(),
+            image: faker.image.avatar(),
+            date: faker.date.past(),
+            description: faker.lorem.paragraph(3),
+        },
+        {
+            id: 2,
+            name: faker.name.findName(),
+            email: faker.internet.email(),
+            image: faker.image.avatar(),
+            date: faker.date.past(),
+            description: faker.lorem.paragraph(3),
+        },
+        {
+            id: 3,
+            name: faker.name.findName(),
+            email: faker.internet.email(),
+            image: faker.image.avatar(),
+            date: faker.date.past(),
+            description: faker.lorem.paragraph(3),
+        },
+    ];
 
     ngOnInit() {}
 }
